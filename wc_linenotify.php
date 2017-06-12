a2061a2f711042d64bd77f8c546857b80a3fc1a8
iamapinan <iamapinan@gmail.com>
2017-06-12 15:54:13 +0700

    first commit to initial the project.

---
+++ wc_linenotify.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Woocommerce Line Notify
+Plugin URI:  https://github.com/iamapinan/wc_linenotify
+Description: Woocommerce new order notify to line chat.
+Version:     1.0
+Author:      Apinan Woratrakun
+Author URI:  apinu.com
+License:     GNU General Public License v3.0
+License URI: https://github.com/iamapinan/wc_linenotify/blob/master/LICENSE
+Text Domain: wc_linenotify
+Domain Path: /languages
+*/
+
