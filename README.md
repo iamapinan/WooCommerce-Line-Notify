39a01bc9bf3fb12b2370efc8ee152ec543b51e49
Apinan Woratrakun <iamapinan@gmail.com>
2018-09-17 21:54:02 +0700

    update v1.0.4

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 ![Woocommerce Line Notify](https://github.com/iamapinan/wc_linenotify/raw/master/src/image/wc_line.png)
-# Woocommerce Line Notify plugin stable version
-Woocommerce new order notify to line notify it's very easy. 
-- [Download](https://github.com/iamapinan/wc_linenotify/archive/master.zip) zip file here.
+# Woocommerce Line Notify
+Woocommerce order notification to Line notify API.
+
+[Download](https://github.com/iamapinan/wc_linenotify/archive/master.zip) zip file here.
+
+## v1.0.4 Changes log
+- Add image to notification.
+- Choose order status to send notification.
+- Show timezone setting in your system.
+- Fixed timezone [issue](https://github.com/iamapinan/wc_linenotify/issues/1).
+- Imploved perfomance by query database when need only.
+- Smooth design settings ui.
+- Support scss style editing.
 
 ## v1.0.3 Changes log
 - Fixed time display in alert message.
@@ -9,11 +19,16 @@ Woocommerce new order notify to line notify it's very easy.
 - Remove state name.
 - Add [order_customer] short code.
 
+## Screenshot
+![Setting](src/screenshot.png) Setting page
+![Alert message](src/result_ss.jpg) Notification page
+
 ## Features
 - Add line token.
 - Message pattern with order short code supported.
 - Send notify to line group or user.
 - Send notify when have order activity.
+- Add logo or image banner to notification.
 
 ## What's benefit of line notify can do.
 1. Send a notification to your self.
@@ -24,7 +39,26 @@ Woocommerce new order notify to line notify it's very easy.
 1. Upload the entire wc_linenotify folder to the /wp-content/plugins/ directory.
 2. Activate the plugin through the ‘Plugins’ menu in WordPress.
 
+## Contibute guidelines
+Thank you for your suggestion and support to the project. I very happy to help and give the thing I can do to the world. If you can help me to develop this plugin or join as a team members please follow below. I'll add your name to contributor members if you've joined.
+1. Clone this repo on development branch.
+2. Make change to the project.
+3. Push update to your branch.
+4. Create pull request with update details to developer.
+5. Waiting for approved.
+
+### Welcome to the opensource world.
+
+## Wiki
 [WIKI](https://github.com/iamapinan/wc_linenotify/wiki)
+
+## Issue
+[Issue](https://github.com/iamapinan/wc_linenotify/issues)
+
+## Any support I can do.
+* Email: [apinan@iotech.co.th](mailto:apinan@iotech.co.th)
+* LineID: iamapinan
+* Facebook: [facebook.com/9apinan](https://www.facebook.com/9apinan)
 
 ## License
 [GNU General Public License v3.0](https://github.com/iamapinan/wc_linenotify/blob/master/LICENSE)
--- wc_linenotify.php
+++ wc_linenotify.php
@@ -3,9 +3,9 @@
 Plugin Name: Woocommerce Line Notify
 Plugin URI:  https://github.com/iamapinan/wc_linenotify
 Description: Woocommerce new order notify to line chat.
-Version:     1.0.3
+Version:     1.0.4
 Author:      Apinan Woratrakun
-Author URI:  apinu.com
+Author URI:  https://iotech.co.th
 License:     GNU General Public License v3.0
 License URI: https://github.com/iamapinan/wc_linenotify/blob/master/LICENSE
 Text Domain: wc_linenotify
@@ -26,7 +26,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
         // @return void
         public function __construct() {
             $this->id = 'wc-linenotify';
-            $this->version  = '1.0.3';
+            $this->version  = '1.0.4';
             $this->author = '<a href="https://facebook.com/9apinan" target="_blank">Apinan Woratrakun</a>';
             $this->title = 'Woocommerce Line Notify';
             $this->notify_api_endpoint = 'https://notify-api.line.me/api/notify';
@@ -39,12 +39,16 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
                 "[order_address]",
                 "[order_customer]"
             ];
-            $this->AlertText = 'Null';
-            $this->wc_line_notify_options = get_option( '_option_name' ); 
+
+            $this->AlertText = null;
+            // System initial.
             $this->init();
         }
-
+        /**
+         * Initial main process.
+         */
         function init() {
+            // Trigger when order status changes.
             add_action( 'woocommerce_order_status_changed', array( $this, 'SendAlert'), 10, 2 );
 
             // Initial plugin and plugin events.
@@ -52,47 +56,76 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
             add_action( 'admin_init', array( $this, 'wc_line_notify_page_init' ) );
         }
 
+        /**
+         * Message pattern improvement.
+         */
         function SendAlert ( $order_id ) {
+            // Get order info.
             $order = wc_get_order( $order_id );
             $the_data = $order->get_data(); // The Order data
 
-            // Pattern convert.
-            $message = $this->wc_line_notify_options['pattern'];
-            $message = str_replace('[order_id]', $order_id, $message);
-            $message = str_replace('[order_status]', $the_data['status'], $message);
-            $message = str_replace('[order_time]', date('d/m/Y, H:i', $the_data['date_created']->getTimestamp()), $message);
-            $message = str_replace('[order_total]', $the_data['total'], $message);
-            $message = str_replace('[order_payment]', $the_data['payment_method_title'], $message);
-            $message = str_replace('[order_customer]', $the_data['billing']['first_name'] .' '. $the_data['billing']['last_name'], $message);
-            $message = str_replace('[order_address]', $the_data['billing']['address_1'] . ' ' . $the_data['billing']['address_2'] . ' ' . $the_data['billing']['city'] , $message);
-            $this->AlertText = $message;
-            $this->SendNotify();
+            // Get plugin options
+            $this->wc_line_notify_options = get_option( '_option_name' );
+            
+            // Check notify rule
+            $availableStatus = json_decode( $this->wc_line_notify_options['status'] );
+            if( in_array( 'wc-' . $the_data['status'], $availableStatus) ) {
+
+                // Pattern convert.
+                $message = $this->wc_line_notify_options['pattern'];
+                $message = str_replace('[order_id]', $order_id, $message);
+                $message = str_replace('[order_status]', $the_data['status'], $message);
+                $message = str_replace('[order_time]', $the_data['date_modified']->date('d/m/Y, H:i'), $message);
+                $message = str_replace('[order_total]', $the_data['total'], $message);
+                $message = str_replace('[order_payment]', $the_data['payment_method_title'], $message);
+                $message = str_replace('[order_customer]', $the_data['billing']['first_name'] .' '. $the_data['billing']['last_name'], $message);
+                $message = str_replace('[order_address]', $the_data['billing']['address_1'] . ' ' . $the_data['billing']['address_2'] . ' ' . $the_data['billing']['city'] , $message);
+                $this->AlertText = $message;
+                $this->SendNotify();
+            }
         }
 
+        /**
+         * Send notify action.
+         */
         public function SendNotify() {
-            // Post to line notify server.
+            // Get plugin options
+            $this->wc_line_notify_options = get_option( '_option_name' );
+
+            // Header setup
             $headers = array( 
                     'Authorization' => 'Bearer '.$this->wc_line_notify_options['token'],
                 );
-
+            // Request prepare
             $args = array(
                 'method' => 'POST',
                 'timeout' => 45,
                 'httpversion' => '1.0',
                 'headers' => $headers,
-                'body' => array( 'message' => $this->AlertText ),
+                'body' => array( 
+                    'message' => $this->AlertText, 
+                    'imageFullsize' => (!empty($this->wc_line_notify_options['image'])) ? $this->wc_line_notify_options['image'] : '', 
+                    'imageThumbnail' => (!empty($this->wc_line_notify_options['image'])) ? $this->wc_line_notify_options['image'] : ''
+                ),
             );
+            // Post to line notify server.
             $response = wp_remote_post( $this->notify_api_endpoint, $args );
+
+            // Debug here.
             // if ( is_wp_error( $response ) ) {
             //     $error_message = $response->get_error_message();
             //     echo "Something went wrong: $error_message";
             //  } else {
             //     echo 'Response:<pre>';
-            //     print_r( $response );
+            //     print_r( $args );
+            //     // print_r( $response );
             //     echo '</pre>';
             //  }
         }
         
+        /**
+         * Initial admin menu page.
+         */
         public function wc_line_notify_add_plugin_page() {
             add_menu_page(
                 'Woocommerce Line Notify', // page_title
@@ -106,8 +139,10 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
             
         }
 
+        /**
+         * Admin settings page output.
+         */
         public function _admin_page() {
-
             ?>
 
             <div class="wrap">
@@ -119,7 +154,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
                     settings_errors();
                 ?>
                 
-                <form method="post" action="options.php">
+                <form method="post" action="options.php" id="WCLineNotifySettings">
                     <?php
                         settings_fields( '_option_group' );
                         do_settings_sections( 'notify-admin' );
@@ -131,7 +166,16 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
             </div>
         <?php }
 
+        /**
+         * Setting fields setup and stylesheet register.
+         */
         public function wc_line_notify_page_init() {
+            // Get plugin options
+            $this->wc_line_notify_options = get_option( '_option_name' );
+
+            wp_register_style( 'wcl_style', plugin_dir_url( __FILE__ ) . 'src/wc_linenotify_style.css' );
+            wp_enqueue_style('wcl_style');
+
             register_setting(
                 '_option_group', // option_group
                 '_option_name', // option_name
@@ -160,16 +204,52 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
                 'notify-admin', // page
                 '_setting_section' // section
             );
+
+            add_settings_field(
+                'image', // id
+                'Add your image url', // title
+                array( $this, 'image_callback' ), // callback
+                'notify-admin', // page
+                '_setting_section' // section
+            );
+
+            add_settings_field(
+                'status', // id
+                'Notify me only', // title
+                array( $this, 'order_status_callback' ), // callback
+                'notify-admin', // page
+                '_setting_section' // section
+            );
+
+            add_settings_field(
+                'timezone', // id
+                'Time zone setting', // title
+                array( $this, 'timezone_callback' ), // callback
+                'notify-admin', // page
+                '_setting_section' // section
+            );
+            
         }
 
+        /**
+         * Sanitize settings data before save it.
+         */
         public function _sanitize($input) {
             $sanitary_values = array();
             if ( isset( $input['token'] ) ) {
                 $sanitary_values['token'] = sanitize_text_field( $input['token'] );
             }
 
+            if ( isset( $input['image'] ) ) {
+                $sanitary_values['image'] = sanitize_text_field( $input['image'] );
+            }
+
             if ( isset( $input['pattern'] ) ) {
                 $sanitary_values['pattern'] = esc_textarea( $input['pattern'] );
+            }
+
+            if ( isset( $input['status'] ) ) {
+                $sanitary_values['status'] = json_encode($input['status']);
             }
 
             return $sanitary_values;
@@ -186,16 +266,39 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
             );
         }
 
+        public function image_callback() {
+            printf(
+                '<input class="regular-text" type="text" name="_option_name[image]" placeholder="https://..." id="image" value="%s" required> <span class="textReq">Support jpg, png Max size: 1024x1024px</span>',
+                isset( $this->wc_line_notify_options['image'] ) ? esc_attr( $this->wc_line_notify_options['image']) : ''
+            );
+        }
+
         public function pattern_callback() {
             printf(
-                '<textarea class="large-text" maxlength="500" placeholder="คุณมีคำสั่งซื้ออยู่ในสถานะ [order_status]" rows="5" name="_option_name[pattern]" id="pattern" style="max-width: 580px;">%s</textarea><br>Max charactor length 500<br> Supported short code: ' . join(', ', $this->patterns),
+                '<textarea class="large-text" maxlength="500" placeholder="คุณมีคำสั่งซื้ออยู่ในสถานะ [order_status]" rows="5" name="_option_name[pattern]" id="pattern" style="max-width: 580px;">%s</textarea><br>Max charactor length <span class="textReq">500</span><br> Supported short code: <span class="shortcodeBadge">' . join(', ', $this->patterns) . '</span>',
                 isset( $this->wc_line_notify_options['pattern'] ) ? esc_attr( $this->wc_line_notify_options['pattern']) : ''
             );
         }
 
+        public function timezone_callback() {
+            $timezone_name =  (get_option( 'gmt_offset' )>0 ) ? '+' . get_option( 'gmt_offset' ) : '-'. get_option( 'gmt_offset' );
+            echo 'UTC' . $timezone_name . ' <a href="/wp-admin/options-general.php">Change your setting</a>';
+        }
+
+        public function order_status_callback() {
+            $statusSelected = json_decode( $this->wc_line_notify_options['status'] );
+
+            $orderStatus = wc_get_order_statuses();
+            foreach($orderStatus as $sid => $sv) {
+
+                $isCheck = (in_array($sid, $statusSelected)) ? 'checked' : '';
+                echo "<span class='order_status_check'><input type='checkbox' name='_option_name[status][]' $isCheck value='$sid' id='status_$sid'> $sv</span>&nbsp;&nbsp;";
+            }
+        }
+
     }
 
-
+    // Start service.
     $wc_notify = new wc_linenotify();
 }
 
